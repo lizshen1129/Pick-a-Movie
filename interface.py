d4491f91433d291f372444a783ff7caef43cf55b
Yang Shen <lizshen@umich.edu>
2021-12-12 17:08:59 -0500

    Added interface.py

---
+++ interface.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+from handle_form import get_inputs, handle_years, handle_multiples_one_type, handle_all_intersection
+from graph import load_graph, save_graph, result_dict
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('pick_a_movie.html')
+
+@app.route('/handle_form', methods=['POST'])
+def handle_the_form():
+    g = load_graph()
+    save_graph(g)
+    input_dict = get_inputs(request.form)
+    year_list = handle_years(input_dict['from_year'], input_dict['to_year'], g)
+    list_of_lists = [year_list]
+    for feature in input_dict.keys():
+        if feature != 'from_year' and feature != 'to_year':
+            if input_dict[feature] and input_dict[feature] != []:
+                feature_list = handle_multiples_one_type(input_dict[feature], feature, g)
+                list_of_lists.append(feature_list)
+    result_list = handle_all_intersection(list_of_lists)
+    if not result_list:
+        result_list = [random.randint(0, 249) for _ in range(9)]
+        rand = True
+    else:
+        rand = False
+    result = result_dict(result_list)
+    return render_template('response.html', result=result, rand=rand)
+
+
+if __name__ == '__main__':  
+    print('starting Flask app', app.name)
+    app.run(debug=True)
